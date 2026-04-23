dca7d2e444c2cc004ab9970c4cce294f3e33d236
dmagallanes2 <89612925+dmagallanes2@users.noreply.github.com>
2026-04-23 08:03:50 -0700

    Add EQTestQuestion and UserEQTestResult entities

---
+++ DuckyEQ.Domain/Entities/EQTestQuestion.cs
@@ -0,0 +1,17 @@
+﻿using DuckyEQ.Domain.Enums;
+
+namespace DuckyEQ.Domain.Entities;
+
+public class EQTestQuestion
+{
+    public Guid Id { get; private set; }
+    public PillarId PillarId { get; private set; }
+    public string QuestionText { get; private set; } = null!;
+    public string OptionA { get; private set; } = null!;
+    public string OptionB { get; private set; } = null!;
+    public string OptionC { get; private set; } = null!;
+    public string OptionD { get; private set; } = null!;
+    public string CorrectAnswer { get; private set; } = null!;
+
+    private EQTestQuestion() { }
+}
---
+++ DuckyEQ.Domain/Entities/UserEQTestResult.cs
@@ -0,0 +1,15 @@
+﻿using DuckyEQ.Domain.Enums;
+
+namespace DuckyEQ.Domain.Entities;
+
+public class UserEQTestResult
+{
+    public Guid Id { get; private set; }
+    public Guid UserId { get; private set; }
+    public PillarId PillarId { get; private set; }
+    public int Score { get; private set; }
+    public int TotalQuestions { get; private set; }
+    public DateTime CompletedAt { get; private set; }
+
+    private UserEQTestResult() { }
+}
